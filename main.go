79434fde348c509b8af965cd2b552d0d21d09d51
caljones <calnickjones@gmail.com>
2026-02-26 15:42:50 -0500

    Nearly finished product!

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ package main
 // different things about what I have learned about Go so far.
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"math/rand/v2"
+	"net/http"
 	"os"
 	"time"
 )
 
+const cali_url = "http://services.explorecalifornia.org/json/tours.php"
+
 func main() {
 	inputText := readFile("./input.txt")
-	typePrint(inputText, 70)
+	typePrint(inputText, 120)
 }
 
 func readFile(fileName string) string {
@@ -28,24 +33,35 @@ func typePrint(input string, speed int) {
 	r := rand.New(rand.NewPCG(0, 1))
 	for _, c := range input {
 
-		if string(c) == "%" {
+		switch {
+		// Print Good Job
+		case string(c) == "$":
 			typePrint(readFile("./good_job.txt"), 2)
-			continue
-		} else if string(c) == "#" {
-			typePrint(readFile("./notion.txt"), 2)
-			continue
-		}
-
-		if string(c) == "@" {
+		// Print Cali HTTP call
+		case string(c) == "#":
+			client := http.Client{}
+			req, err := http.NewRequest("GET", cali_url, nil)
+			checkErr(err)
+			req.Header.Set("User-Agent", "")
+			resp, err := client.Do(req)
+			checkErr(err)
+			bytes, err := io.ReadAll(resp.Body)
+			checkErr(err)
+			fmt.Println(string(bytes))
+			resp.Body.Close()
+		// Delay half a second
+		case string(c) == "@":
 			time.Sleep(time.Second / 2)
-			continue
-		} else if string(c) == "^" {
+		// Backspace
+		case string(c) == "^":
 			typeDelete()
 			time.Sleep(time.Duration(r.IntN(speed)) * time.Millisecond)
-			continue
+		// Dance animation
+		case string(c) == "&":
+			dance()
+		default:
+			fmt.Print(string(c))
 		}
-
-		fmt.Print(string(c))
 		time.Sleep(time.Duration(r.IntN(speed)) * time.Millisecond)
 	}
 }
@@ -53,6 +69,30 @@ func typePrint(input string, speed int) {
 func typeDelete() {
 	fmt.Print("\b \b")
 	time.Sleep(50 * time.Millisecond)
+}
+
+func dance() {
+	file, err := os.Open("./dance.txt")
+	checkErr(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	counter := 0
+	for true {
+		for range 5 {
+			scanner.Scan()
+			s := scanner.Text()
+			if string(s[len(s)-1]) == "#" {
+				goto end
+			}
+			fmt.Println(s)
+			counter++
+		}
+		time.Sleep(150 * time.Millisecond)
+		for range 5 {
+			fmt.Printf("\033[1A\033[K")
+		}
+	}
+end:
 }
 
 func checkErr(err error) {
